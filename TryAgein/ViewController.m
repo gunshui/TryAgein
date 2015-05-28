be607d3056c2b2796e0de1ecdb6844d027a4f33a
Gunshui <minqinyang@163.com>
2015-05-28 16:51:24 +0800

    qqqqqqqqqq

--- TryAgein/ViewController.m
+++ TryAgein/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"ce shi yi ci ");
 }
 
 - (void)didReceiveMemoryWarning {
